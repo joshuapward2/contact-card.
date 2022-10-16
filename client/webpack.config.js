1fff5fc501859959a13377843a30e5b5d8c22de9
joshua.p.ward2 <joshua.p.ward2@gmail.com>
2022-10-16 15:22:57 -0400

    fixed package.json

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -7,12 +7,13 @@ const path = require('path');
 
 
 module.exports = {
+  mode: "production",
     entry: "./src/js/index.js",
     output: {
         filename: "bundle.js",
         path: path.resolve(__dirname, "dist"),
     },
-    mode: "development",
+  
     module: {
         rules: [
           {
