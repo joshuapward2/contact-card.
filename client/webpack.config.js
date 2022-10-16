3c41bf87d490d4f12fbb0f8ee82c6729813ffdf1
joshua.p.ward2 <joshua.p.ward2@gmail.com>
2022-10-16 00:56:04 -0400

    lesson 4 done

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
  // Webpack plug-ins
  const HtmlWebpackPlugin = require('html-webpack-plugin');
+ const WorkboxPlugin = require('workbox-webpack-plugin');
+ const {InjectManifest} = require('workbox-webpack-plugin');
 
 
 
@@ -41,8 +43,14 @@ module.exports = {
           new HtmlWebpackPlugin({
             template: './index.html',
             title: 'Webpack Plugin',
-          })
+          }),
+          new InjectManifest({
+            swSrc: './src/sw.js',
+            swDest: 'service-worker.js',
+          }),
+          
         ]
       
 }
+
 
